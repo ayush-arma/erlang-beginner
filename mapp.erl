84b6eda39d76604d281e31b199731f53aabb1ef5
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-05-24 15:37:11 +0530

    maps changes

--- mapp.erl
+++ mapp.erl
@@ -1,5 +1,6 @@
 -module(mapp).
 -export([freqCount/1]).
+-export([merge/2]).
 
 
 
@@ -17,3 +18,19 @@ freqCount([E|LIST],Map)->
         
 
         %mapp:freqCount([a,a,a,a,a,a,b,b,b,c,c,c,c,d,e,f,r,w,w,w,wwww]).
+
+merge(Map1, Map2)->
+    Keys=maps:keys(Map2),
+    merge(Map1,Map2,Keys).
+
+merge(Map1,_Map2,[])->
+    Map1;
+merge(Map1,Map2,[K|LISTT])->
+    Value2=maps:get(K,Map2),
+    Value1=maps:get(K,Map1,0),
+    NewMap1=Map1#{K => Value1+Value2},
+    merge(NewMap1,Map2,LISTT).
+
+
+%mapp:merge(    #{a => 2, b => 3},    #{a => 5, c => 1}).
+
