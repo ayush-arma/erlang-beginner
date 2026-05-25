6d2b7ecbabf276a97119a872b761db24326ccd8d
AYUSH SHARMA <ayushsharma@AYUSHs-MacBook-Air-2.local>
2026-05-25 22:47:49 +0530

    Expceitons

--- error_hand/trycatch.erl
+++ error_hand/trycatch.erl
@@ -1,5 +1,5 @@
 -module(trycatch).
--export([safe_divide/2]).
+-export([safe_divide/2,safe_endd/0]).
 
 safe_divide(X, Y) ->
     try X / Y of
@@ -9,4 +9,14 @@ safe_divide(X, Y) ->
             {error, division_by_zero};
         Class:Reason:Stack ->
             {error, {caught, Class, Reason, Stack}}
+    end.
+
+endd()->
+    exit(testing).
+
+safe_endd() ->
+    try endd() of
+        _ -> ok
+    catch
+        exit:Reason -> {caught_exit, Reason}
     end.
