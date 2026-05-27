19f3ef53ccdaaf46ba77d8b1bc7f7cd882d47c0a
Ayush Sharma <58801471+ayush-arma@users.noreply.github.com>
2026-05-27 10:02:40 +0530

    Calc

--- stack-calc/rpn_calc.erl
+++ stack-calc/rpn_calc.erl
@@ -3,31 +3,31 @@
 
 calc(Str) ->
     Tokens = string:tokens(Str, " "),
-    Stack = eval_tokens(Tokens, []),
+    Stack = calc(Tokens, []),
     case Stack of
         [Result] -> Result;
         _ -> {error, invalid_expression}
     end.
 
-eval_tokens([], Stack) ->
+calc([], Stack) ->
     Stack;
 
-eval_tokens(["+" | Rest], [B, A | Stack]) ->
-    eval_tokens(Rest, [A + B | Stack]);
+calc(["+" | Rest], [B, A | Stack]) ->
+    calc(Rest, [A + B | Stack]);
 
-eval_tokens(["-" | Rest], [B, A | Stack]) ->
-    eval_tokens(Rest, [A - B | Stack]);
+calc(["-" | Rest], [B, A | Stack]) ->
+    calc(Rest, [A - B | Stack]);
 
-eval_tokens(["*" | Rest], [B, A | Stack]) ->
-    eval_tokens(Rest, [A * B | Stack]);
+calc(["*" | Rest], [B, A | Stack]) ->
+    calc(Rest, [A * B | Stack]);
 
-eval_tokens(["/" | Rest], [B, A | Stack]) ->
-    eval_tokens(Rest, [A div B | Stack]);
+calc(["/" | Rest], [B, A | Stack]) ->
+    calc(Rest, [A div B | Stack]);
 
-eval_tokens([Token | Rest], Stack) ->
+calc([Token | Rest], Stack) ->
     case string:to_integer(Token) of
         {Int, ""} ->
-            eval_tokens(Rest, [Int | Stack]);
+            calc(Rest, [Int | Stack]);
         _ ->
             {error, {invalid_token, Token}}
     end.
