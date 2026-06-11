fca8c46ed92c1a0c2968a2f51d93348a8d2386ce
AYUSH SHARMA <ayushsbitw@gmail.com>
2026-06-11 20:24:01 +0530

    changed counter to include history

--- gen_server/counter/counter_server.erl
+++ gen_server/counter/counter_server.erl
@@ -1,42 +1,39 @@
 -module(counter_server).
--behaviour(gen_server). %% Tells the compiler this module implements gen_server callbacks
+-behaviour(gen_server). 
 
--export([start_link/0, increment/0, get_value/0,get_name/0]).
+-export([start_link/0, increment/0, reset/0,get_value/0, get_history/0,increment/1]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2
+]).
 
-
-start_link() ->
+start_link()->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-increment() ->
-    gen_server:cast(?MODULE, increment).
+init([])->
+    {ok,[0]}.
 
-get_value() ->
-    gen_server:call(?MODULE, get_value).
-init([]) ->
-    InitialState = 0,
-    {ok, InitialState}.
+reset()->
+    gen_server:cast(?MODULE, reset).
 
-handle_call(get_value, _From, State) ->
-    {reply, State, State}; 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+increment()->
+    increment(1).
+increment(Step)->
+    gen_server:cast(?MODULE, {inc,Step}).
 
-handle_cast(increment, State) ->
-    {noreply, State + 1};  
+get_value()->
+    gen_server:call(?MODULE, first).
 
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+get_history()->
+    gen_server:call(?MODULE,history).
 
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_cast({inc,Step},State=[First|_Rest])->
+    NewState=[First+Step|State],
+    {noreply,NewState};
+handle_cast(reset,_State)->
+    {noreply,[0]}.
 
-terminate(_, _) ->
-    ok.
+handle_call(first,_From,State=[First|_Rest])->
+    {reply,First,State};
+handle_call(history,_From,State)->
+    {reply,State,State}.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-get_name()->
-    io:format("Error in module: ~p~n", [?MODULE]).
